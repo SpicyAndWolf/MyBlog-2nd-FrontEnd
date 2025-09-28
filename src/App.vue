443c3a0d3650d3b104987f2bbbeb9be88ab64d9d
SpicyAndWolf <heluoshiyongyuandishen@gmail.com>
2025-09-28 21:38:40 +0800

    home-laptop-2025-9-28-21:38

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,76 @@
 <script setup>
 import Navigation from "./components/Navigation.vue";
+import rainGif from "@/assets/rain.jfif";
+import avatar from "@/assets/avatar.JPG";
 </script>
 
 <template>
-  <Navigation></Navigation>
+  <div class="app-layout">
+    <Navigation></Navigation>
+    <main class="main-content">
+      <section class="user-info">
+        <div class="rain-gif">
+          <img :src="rainGif" />
+        </div>
+        <div class="avatar">
+          <img :src="avatar" />
+        </div>
+        <nav class="contact-links">
+          <a href="/">bilibili</a>
+          <a href="/">bangumi</a>
+        </nav>
+      </section>
+      <section class="recent-articles">
+        <h2>Recent Articles</h2>
+        <ul>
+          <li><a href="/article1">文章1</a></li>
+          <li><a href="/article2">文章2</a></li>
+          <li><a href="/article3">文章3</a></li>
+        </ul>
+      </section>
+    </main>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+.main-content {
+  display: flex;
+  flex-direction: row;
+  flex: 1;
+}
+
+.user-info {
+  display: flex;
+  width: 300px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.rain-gif {
+  width: 80%;
+}
+
+.rain-gif img {
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+}
+
+.avatar {
+  width: 80%;
+}
+
+.avatar img {
+  width: 100%;
+  aspect-ratio: 1;
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,35 @@
+<script setup>
+import myLogo from "@/assets/logo.jpg";
+</script>
+
 <template>
-  <nav>
-    <a href="/">SpiceNest</a>
+  <nav class="navigation">
+    <div class="logo-container">
+      <img :src="myLogo" alt="" />
+      <span>SpiceNest</span>
+    </div>
     <a href="/article">文章</a>
   </nav>
 </template>
+
+<style scoped>
+.navigation {
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.logo-container {
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+  margin-left: 20px;
+}
+
+.logo-container img {
+  height: 100%;
+}
+</style>
